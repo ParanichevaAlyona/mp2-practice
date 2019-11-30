6d0168c794db02c416c8c34b9123a9363dc6715c
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2019-11-30 23:51:44 +0300

    Update main.cpp

--- List/List/main.cpp
+++ List/List/main.cpp
@@ -10,15 +10,51 @@ void main()
 	char* d = "dela";
 	char* m = "Masha";
 
-	TNode<char, int>  a(1, p, 0);
-	TNode<char, int>  b(2, k, 0);
-	TNode<char, int>  c(3, d, 0);
-	TNode<char, int>  e(4, m, 0);
-	TList<char, int> list;
+	TNode<char, int>  privet(1, p, 0);
+	TNode<char, int>  kak(2, k, 0);
+	TNode<char, int>  dela(3, d, 0);
+	TNode<char, int>  Masha(4, m, 0);
 
-	list.Push(a.key, a.pData);
-	list.Back(b.key, b.pData);
-	list.InsertAfter(b.key, c.pData, c.key);
-	list.InsertBefore(a.key, e.pData, e.key);
-	list.Remove(4);
+	TList<char, int> list;
+	//Back
+	list.Back(kak.key, kak.pData);
+	//Push
+	list.Push(privet.key, privet.pData);
+	//InsertAfter
+	try
+	{
+		list.InsertAfter(kak.key, dela.pData, dela.key);
+	}
+	catch(const char* s)
+	{
+		cout << s << endl;
+	}
+	
+	//InsertBefore
+	try
+	{
+		list.InsertBefore(privet.key, Masha.pData, Masha.key);
+	}
+	catch(const char* s)
+	{
+		cout << s << endl;
+	}
+
+	list.Print();
+	try/////catch the exception
+	{
+		list.InsertBefore(10, Masha.pData, Masha.key);
+	}
+	catch(const char* s)
+	{
+		cout << s << endl;
+	}
+	try/////catch the exception
+	{
+		list.InsertAfter(10, dela.pData, dela.key);
+	}
+	catch(const char* s)
+	{
+		cout << s << endl;
+	}
 }
